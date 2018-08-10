2a7fea4968ae28fc2d76537d0018ec3294319aff
Ben Tomasini <btomasini@activated.io>
2018-08-09 20:25:36 -0700

    Completed initial ansible hosts plugin

--- builtin/plugins/ansiblehosts/plugin.go
+++ builtin/plugins/ansiblehosts/plugin.go
@@ -3,6 +3,20 @@ package ansiblehosts
 import (
 	"sync"
 
+	"strings"
+
+	"errors"
+
+	"os/exec"
+
+	"encoding/json"
+
+	"os"
+
+	"fmt"
+	"regexp"
+	"sort"
+
 	"github.com/activatedio/wrangle/plugin"
 )
 
@@ -51,6 +65,11 @@ type AnsibleHostsPlugin struct {
 	Config     *AnsibleHostsPluginConfig
 }
 
+type entry struct {
+	name string
+	ip   string
+}
+
 func (self *AnsibleHostsPlugin) GetConfig() interface{} {
 
 	self.ConfigLock.Lock()
@@ -64,6 +83,91 @@ func (self *AnsibleHostsPlugin) GetConfig() interface{} {
 }
 
 func (self *AnsibleHostsPlugin) Filter(c plugin.Context) error {
+
+	f, err := os.Create("./hosts")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	p := c.GetGlobalContext().Delegate.Path
+
+	if !strings.HasSuffix(p, "terraform") {
+		return errors.New("Executable must be terraform")
+	}
+
+	conf := self.Config
+
+	var entries []*entry
+
+	for _, m := range conf.Modules {
+		cmd := exec.Command(p, "-module="+m, "-json")
+		out, err := cmd.Output()
+		if err != nil {
+			return err
+		}
+		j := make(map[string]interface{})
+		err = json.Unmarshal(out, &j)
+		if err != nil {
+			return err
+		}
+
+		if _, ok := j[conf.FqdnOutputName]; !ok {
+			break
+		}
+		if _, ok := j[conf.RecordsOutputName]; !ok {
+			break
+		}
+
+		fqdns := j[conf.FqdnOutputName].(map[string]interface{})["value"].([]interface{})
+		records := j[conf.RecordsOutputName].(map[string]interface{})["value"].([]interface{})
+
+		for i, fqdn := range fqdns {
+
+			entries = append(entries, &entry{
+				name: fqdn.(string),
+				ip:   records[i].([]interface{})[0].(string),
+			})
+		}
+	}
+
+	if len(entries) > 0 {
+		f.WriteString("[all]\n")
+	}
+
+	for _, entry := range entries {
+		f.WriteString(fmt.Sprintf("%s ansible_host=%s\n", entry.name, entry.ip))
+	}
+
+	if conf.Groups != nil {
+
+		var keys []string
+		for k := range conf.Groups {
+			keys = append(keys, k)
+		}
+
+		sort.Strings(keys)
+
+		for _, k := range keys {
+
+			v := conf.Groups[k]
+
+			f.WriteString(fmt.Sprintf("\n[%s]\n", k))
+
+			for _, entry := range entries {
+				match, err := regexp.Match(v.Pattern, []byte(entry.name))
+				if err != nil {
+					return err
+				}
+				if match {
+					f.WriteString(entry.name + "\n")
+				}
+			}
+
+		}
+	}
+
+	c.Next()
 
 	return nil
 }
--- builtin/plugins/ansiblehosts/plugin_test.go
+++ builtin/plugins/ansiblehosts/plugin_test.go
@@ -4,12 +4,114 @@ import (
 	"strings"
 	"testing"
 
+	"os"
+	"path/filepath"
+	"reflect"
+
+	"os/exec"
+
+	"io/ioutil"
+
 	"github.com/activatedio/wrangle/config"
+	"github.com/activatedio/wrangle/context"
+	"github.com/activatedio/wrangle/e2e"
 	"github.com/activatedio/wrangle/plugin"
 )
 
 var _ (plugin.Plugin) = (*AnsibleHostsPlugin)(nil)
 
+func TestTemplatePlugin_Filter(t *testing.T) {
+
+	cases := map[string]struct {
+		config *AnsibleHostsPluginConfig
+		err    error
+	}{
+		"empty-output": {
+			config: &AnsibleHostsPluginConfig{
+				Modules: []string{"a"},
+			},
+		},
+		"two-instances": {
+			config: &AnsibleHostsPluginConfig{
+				Modules:           []string{"a"},
+				FqdnOutputName:    "instance_fqdns",
+				RecordsOutputName: "instance_records",
+			},
+		},
+		"instances-with-groups": {
+			config: &AnsibleHostsPluginConfig{
+				Modules:           []string{"a"},
+				FqdnOutputName:    "instance_fqdns",
+				RecordsOutputName: "instance_records",
+				Groups: map[string]*Group{
+					"build": {"^build"},
+					"ops":   {"^ops"},
+					"zero":  {"0"},
+					"one":   {"1"},
+				},
+			},
+		},
+		"instances-with-groups-two-modules": {
+			config: &AnsibleHostsPluginConfig{
+				Modules:           []string{"a", "b"},
+				FqdnOutputName:    "instance_fqdns",
+				RecordsOutputName: "instance_records",
+				Groups: map[string]*Group{
+					"build": {"^build"},
+					"ops":   {"^ops"},
+					"zero":  {"0"},
+					"one":   {"1"},
+				},
+			},
+		},
+	}
+
+	for k, v := range cases {
+		t.Run(k, func(t *testing.T) {
+			b := e2e.NewBinary("", filepath.Join("test-fixtures", k))
+			orig, err := os.Getwd()
+			if err != nil {
+				t.Fatal("Couldn't get current working directory")
+			}
+			os.Chdir(b.Path())
+			defer os.Chdir(orig)
+
+			u := &AnsibleHostsPlugin{
+				Config: v.config,
+			}
+
+			c := &plugin.StubContext{
+				GlobalContext: &context.Context{
+					Delegate: &exec.Cmd{
+						Path: "./terraform",
+					},
+				},
+			}
+
+			err = u.Filter(c)
+
+			if err != nil && v.err == nil {
+				t.Fatalf("Unexpected error %s", err)
+			} else if !reflect.DeepEqual(err, v.err) {
+				t.Fatalf("Expected [%+v], got [%+v]", v.err, err)
+			}
+
+			if err == nil && c.NextCallCount != 1 {
+				t.Fatalf("Expected next call %d times", 1)
+			}
+
+			expected, err := ioutil.ReadFile("./reference")
+			check(err)
+			got, err := ioutil.ReadFile("./hosts")
+
+			if !reflect.DeepEqual(got, expected) {
+				t.Fatalf("Files do not match, expected \n[%s], got \n[%s]", expected, got)
+			}
+
+		})
+	}
+
+}
 func TestAnsibleHostsPlugin_Config(t *testing.T) {
 
 	plugins := map[string]config.WithConfig{
