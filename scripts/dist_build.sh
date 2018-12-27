e57f24d944ef4a0f90a9ec059e5efef3235078c0
Ben Tomasini <btomasini@activated.io>
2018-12-26 16:07:23 -0800

    Release v0.0.4

--- scripts/dist_build.sh
+++ scripts/dist_build.sh
@@ -11,18 +11,6 @@ cd $DIR
 
 # Make sure build tools are available.
 make tools
-
-# Fixup the timestamps to match what's checked in. This will allow us to cleanly
-# verify that the checked-in content is up to date without spurious diffs of the
-# file mod times.
-pushd pkg
-cat ../command/agent/bindata_assetfs.go | ../scripts/fixup_times.sh
-popd
-
-# Now we are ready to do a clean build of everything. The "all" build will blow
-# away our pkg folder so we have to regenerate the ui once more. This is probably
-# for the best since we have meddled with the timestamps.
-rm -rf pkg
 make all
 chmod 777 pkg
 
